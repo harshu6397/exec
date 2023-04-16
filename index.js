282ef0bc6d59898eb1c7b7cf89f07c741bd594fb
w3villa-harsh-kumar <harsh.kumar@w3villa.com>
2023-04-16 05:35:25 +0530

    Inital Commit

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ app.post("/execute_c_program", function (req, res) {
     if (err) {
       console.log(err);
     }
-  }); 
-   
+  });
+
   const compile_c = async () => {
     return new Promise((resolve, reject) => {
-      exec("gcc -o code code.c", async function (error, stdout, stderr) { 
+      exec("gcc -o code code.c", async function (error, stdout, stderr) {
         if (error) {
           reject(error);
         } else {
@@ -64,6 +64,51 @@ app.post("/execute_c_program", function (req, res) {
   };
 
   main_c();
+});
+
+app.post("/execute_java_program", function (req, res) {
+  const code = req.body.code;
+
+  // Creating the file
+  fs.writeFile("code.java", code, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  const compile_java = async () => {
+    return new Promise((resolve, reject) => {
+      exec("javac code.java", async function (error, stdout, stderr) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(stdout);
+        }
+      });
+    });
+  };
+
+  const run_java = async () => {
+    return new Promise((resolve, reject) => {
+      exec("java code", async function (error, stdout, stderr) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(stdout);
+        }
+      });
+    });
+  };
+
+  const main_java = async () => {
+    await compile_java();
+    const result = await run_java();
+    fs.unlinkSync("code.java");
+    fs.unlinkSync("code.class");
+    res.send(result);
+  };
+
+  main_java();
 });
 
 // Setting up the port
